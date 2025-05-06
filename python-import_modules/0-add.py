2c77a33d307fd19e83eb8536a6011761f9481282
Delphes1980 <delphine.coutouly-laborda@hotmail.com>
2025-05-06 19:51:35 +0200

    change the code to fix the output

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-def add(a, b):
-    return a + b
-    __name__ == "__main__"
+from add_0 import add
+
+if __name__ == "__main__":
     a = 1
     b = 2
-    print("{:d}".format(a + b))
+    add = a + b
+    print("{:d} + {:d} = {:d}".format(a, b, add))
