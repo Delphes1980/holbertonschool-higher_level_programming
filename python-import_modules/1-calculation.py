59806c04d54abc2ba7da57b24321a0e67348bbee
Delphes1980 <delphine.coutouly-laborda@hotmail.com>
2025-05-06 20:15:08 +0200

    add an alias to calculator import

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-import calculator_1
+import calculator_1 as calc
 
 if __name__ == "__main__":
     a = 10
     b = 5
-    result1 = calculator_1.add(a, b)
+    result1 = calc.add(a, b)
     print("{} + {} = {}".format(a, b, result1))
-    result2 = calculator_1.sub(a, b)
+    result2 = calc.sub(a, b)
     print("{} + {} = {}".format(a, b, result2))
-    result3 = calculator_1.mul(a, b)
+    result3 = calc.mul(a, b)
     print("{} + {} = {}".format(a, b, result3))
-    result4 = calculator_1.div(a, b)
+    result4 = calc.div(a, b)
     print("{} + {} = {}".format(a, b, result4))
