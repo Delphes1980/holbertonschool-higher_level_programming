bae1367c5b92a93f68e7e6a759eeb5108ffddf58
Delphes1980 <delphine.coutouly-laborda@hotmail.com>
2025-05-13 20:43:42 +0200

    restart from the beginning

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -2,5 +2,10 @@
 def best_score(a_dictionary):
     if not a_dictionary:
         return None
-    biggest = max(a_dictionary)
-    return biggest
+    biggest_key = None
+    biggest = -1
+    for key, value in a_dictionary.items():
+        if value > biggest:
+            biggest = value
+            biggest_key = key
+    return biggest_key
