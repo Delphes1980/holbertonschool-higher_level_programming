3d1dd6935f68de8c66e20230d85475774fe23702
Delphes1980 <delphine.coutouly-laborda@hotmail.com>
2025-05-14 16:00:32 +0200

    change the code for the true statement

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-        if value is int() is True:
-            print("{:d}".format(value))
+        print("{:d}".format(value))
+        return True
     except (ValueError, TypeError):
         return False
