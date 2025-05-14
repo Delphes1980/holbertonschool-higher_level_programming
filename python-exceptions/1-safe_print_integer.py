9135b5a383a1658b67aa62a5a59a53a4c29179f2
Delphes1980 <delphine.coutouly-laborda@hotmail.com>
2025-05-14 15:53:02 +0200

    Add 1-safe_print_integer file
    function that prints an integer with {:d}.format().Returns True if value has been correctly printed (it means the value is an integer), Otherwise, returns False

---
+++ python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def safe_print_integer(value):
+    try:
+        if value is int() is True:
+            print("{:d}".format(value))
+    except ValueError:
+        return False
