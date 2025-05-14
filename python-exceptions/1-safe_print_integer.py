88fd05e694cef361ccc880c80b3274bcdb733e7a
Delphes1980 <delphine.coutouly-laborda@hotmail.com>
2025-05-14 15:56:59 +0200

    add TypeError to exception

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -3,5 +3,5 @@ def safe_print_integer(value):
     try:
         if value is int() is True:
             print("{:d}".format(value))
-    except ValueError:
+    except (ValueError, TypeError):
         return False
