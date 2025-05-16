15e7587c5fcb444d76f8ccd16a123fbf8e011cc5
Delphes1980 <delphine.coutouly-laborda@hotmail.com>
2025-05-16 11:12:51 +0200

    fix code error in 4-print_square.py and fix syntax in 4-print_square.txt

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -40,6 +40,6 @@ def print_square(size):
 
     for i in range(size):
         print("#", end='')
-        for j in range(size):
+        for j in range(size - 1):
             print("#", end='')
         print()
