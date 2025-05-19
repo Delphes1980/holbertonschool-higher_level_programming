d1be2baa513c11683e23a59f267431cdaa455388
Delphes1980 <delphine.coutouly-laborda@hotmail.com>
2025-05-19 11:21:50 +0200

    Add the python_classes folder, the readme and 0-square.py files
    an empty class Square that defines a square

---
+++ python-classes/0-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+    Module description: a module that contains a class called Square
+
+    Functions: Class Square
+"""
+
+
+class Square:
+    """ A class Square that defines a square"""
+    pass
