8f4cc1fb10276b62323ddcac87717537a6522d38
Delphes1980 <delphine.coutouly-laborda@hotmail.com>
2025-05-19 12:48:30 +0200

    Add 3-square file
    add the public instance method def area(self): that returns the current square area

---
+++ python-classes/3-square.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python3
+"""
+    Module description: a module that contains a class called Square
+that define a square
+
+    Functions: class Square
+"""
+
+
+class Square:
+    """ A class Square that defines a square
+
+    """
+
+    def __init__(self, size=0):
+        """
+        Instantiation of the size attribute
+
+        Note:
+        __init__ method assign values to object properties
+        size is a private instance attribute
+
+        Args:
+        size (int): size of the square
+
+        Attributes:
+        size (int): size of the square
+
+        Returns:
+        the size of the square
+
+        Raises:
+        TypeError if size is not an integer
+        ValueError is size is less than 0
+        """
+
+        # Check if size is an integer
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+
+        # Check is size is less than 0
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
+
+    def area(self):
+        """
+        Returns the current square area
+
+        Note:
+        area is a public instance method
+
+        Attributes:
+        square_area (int): the area of the square
+        size (int): the size of the square
+
+        Returns:
+        the current square area
+
+        """
+
+        square_area = self.__size * self.__size
+        return square_area
