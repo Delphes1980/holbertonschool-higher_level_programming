c4130d6b4f38cdc2c790a8fedb5d4db98c11be76
Delphes1980 <delphine.coutouly-laborda@hotmail.com>
2025-05-19 19:26:04 +0200

    Add python_more_classes folder, readme and 0-rectangle.py files
    Create a class that define a rectangle

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+This module defines a class Rectangle that represents a rectangle shape.
+"""
+
+
+class Rectangle:
+    """ A class Rectangle that defines a rectangle"""
+    pass
