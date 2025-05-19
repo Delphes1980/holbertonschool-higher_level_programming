974ee5e835119056f124befc43610e2726dc521c
Delphes1980 <delphine.coutouly-laborda@hotmail.com>
2025-05-19 18:45:05 +0200

    add an exception statement in @position.setter

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -82,6 +82,8 @@ class Square:
 
     @position.setter
     def position(self, value):
+        if not isinstance(value, tuple):
+            raise TypeError("position must be a tuple of 2 positive integers")
         if len(value) == 2:
             if not all(isinstance(num, int) and num >= 0 for num in value):
                 raise TypeError("position must be a tuple of 2 positive integers")
