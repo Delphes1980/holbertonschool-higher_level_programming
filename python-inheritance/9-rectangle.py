accd2aa639c13dc907de9457a2213aa8bdc67374
Delphes1980 <delphine.coutouly-laborda@hotmail.com>
2025-05-26 17:01:44 +0200

    Add 9-rectangle file
    Implement area method, add __str__ method

---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""
+This module defines a class BaseGeometry
+"""
+
+# Importation of BaseGeometry class
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """ creation of a class which inherits from BaseGeometry class"""
+
+    def __init__(self, width, height):
+        """
+        A class that defines a rectangle by its height and width
+
+        Note:
+        Rectangle inherits from BaseGeometry
+        width and height are private attributes
+
+        Args:
+        width (int): the width of the rectangle
+        height (int): the height of the rectangle
+        """
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """
+        Calculates and returns the area of the rectangle.
+
+        Note: is a public instance method
+        """
+
+        return self.__width * self.__height
+
+    def __str__(self):
+        """
+        Returns the string representation of the Rectangle object.
+        """
+        return ("[Rectangle] {}/{}".format(self.__width, self.__height))
