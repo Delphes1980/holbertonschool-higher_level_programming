66698a46b324ee57e8f6b4035deb8ba36098e3e5
Delphes1980 <delphine.coutouly-laborda@hotmail.com>
2025-05-26 14:34:21 +0200

    add pass in the method def area

--- python-inheritance/6-base_geometry.py
+++ python-inheritance/6-base_geometry.py
@@ -6,11 +6,10 @@ This module defines a class BaseGeometry
 
 class BaseGeometry:
     """ creation of an empty class"""
-    pass
 
-
-def area(self):
-    """
-    Raise an exception for a non implemented class
-    """
-    raise Exception("area() is not implemented")
+    def area(self):
+        """
+        Raise an exception for a non implemented class
+        """
+        raise Exception("area() is not implemented")
+        pass
