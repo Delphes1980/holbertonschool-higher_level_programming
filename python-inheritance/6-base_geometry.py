a8f72ea062e0a13a38c8bdf3bd879d59a06e8d99
Delphes1980 <delphine.coutouly-laborda@hotmail.com>
2025-05-26 14:31:34 +0200

    Add 6-base_geometry file
    a method def area that raises an Exception with a message 'area() is not implemented'

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+This module defines a class BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """ creation of an empty class"""
+    pass
+
+
+def area(self):
+    """
+    Raise an exception for a non implemented class
+    """
+    raise Exception("area() is not implemented")
