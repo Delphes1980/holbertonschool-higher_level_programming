c2e6662c13dfbdb328ee023c52dfbc8cec7194d4
Delphes1980 <delphine.coutouly-laborda@hotmail.com>
2025-05-26 15:18:23 +0200

    Add 7-base_geometry.py and 7-base_geometry.txt
    a method that checks if value is a positive integer

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+This module defines a class BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """ creation of an empty class"""
+
+    def area(self):
+        """
+        Raise an exception for a non implemented class
+
+        Note: is a public instance method
+        """
+        raise Exception("area() is not implemented")
+        pass
+
+    def integer_validator(self, name, value):
+        """
+        Note:
+        is a public instance method
+        name is always a string
+
+        Raises:
+        TypeError: if value is not an integer
+        ValueError: if value is <= 0
+        """
+
+        # Checks if value is an integer
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+
+        # Checks if value i <= 0
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+        pass
