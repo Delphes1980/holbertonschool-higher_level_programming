1d7c709f4368cba9e9022ce9ac717bd936a95302
Delphes1980 <delphine.coutouly-laborda@hotmail.com>
2025-05-27 18:50:08 +0200

    Add task_03_countediterator file
    A class that iterate in an object using iter() and next() methods

---
+++ python-abc/task_03_countediterator.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+This module defines a class that print a notification message every time an
+item is added (using the append or extend methods) or removed (using the
+remove or pop methods)
+"""
+
+
+class CountedIterator:
+    """  A class that prints a message every time an item is added or removed
+    """
+
+    def __init__(self, iterable):
+        """
+        Initializes the CountedIterator.
+
+        Args:
+        iterable: The object to be iterated
+        It must be an iterable(lists, tuples, dictionaries..)
+        """
+        self.iterator = iter(iterable)  # Initialize the iterator object
+        count = 0  # Initialization of a counter
+
+    def __iter__(self):
+        """ Returns the iterator object itself """
+        return self
+
+    def __next__(self):
+        """ Returns the next item """
+        count = next(self.iterator)  # Goes to the next item in the iterable
+        type(self).count += 1  # Implementation of the counter
+        return count
+
+    def get_count(self):
+        """Returns the current number of iterated elements """
+        return self.count
