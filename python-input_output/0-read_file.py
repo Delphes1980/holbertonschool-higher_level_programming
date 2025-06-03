86df629364d5d6abfcfd45cf762edd9da13d304a
Delphes1980 <delphine.coutouly-laborda@hotmail.com>
2025-06-03 09:18:25 +0200

    add end='' to the print

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -10,4 +10,4 @@ def read_file(filename=""):
     A function that reads a text file (UTF8) and prints it to stdout
     """
     with open(filename, "r", encoding="utf_8") as f:
-        print(f.read())
+        print(f.read(), end='')
