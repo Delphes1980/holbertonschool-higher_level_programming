8bf081ce3963b32c867380d3b497550b47a47db3
Delphes1980 <delphine.coutouly-laborda@hotmail.com>
2025-06-03 09:15:27 +0200

    Add python_input_output folder, readme and 0-read_file files
    A function that reads a file and print it to the stdout

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+This module defines a method that reads a file (UTF8) and prints it to the
+standard output
+"""
+
+
+def read_file(filename=""):
+    """
+    A function that reads a text file (UTF8) and prints it to stdout
+    """
+    with open(filename, "r", encoding="utf_8") as f:
+        print(f.read())
