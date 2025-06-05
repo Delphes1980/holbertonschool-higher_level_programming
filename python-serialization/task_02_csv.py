d48c203bddd2f8b4a4920c2d6279cf310b1b72c9
Delphes1980 <delphine.coutouly-laborda@hotmail.com>
2025-06-05 11:32:06 +0200

    Add some comments and change some code lines in task_02_csv file

--- python-serialization/task_02_csv.py
+++ python-serialization/task_02_csv.py
@@ -1,16 +1,34 @@
 #!/usr/bin/python3
 """
-This module defines a method that serialize and deserialize an objet
-using CSV
+This module defines a function to convert CSV data to JSON format
 """
 
 import csv
 import json
 
+
 def convert_csv_to_json(csv_filename):
-    with open(csv_filename, 'r', newline='', encoding='utf-8') as file_csv:
-        reader_dict = csv.DictReader(file_csv)
-    with open('reader_dict', 'w', encoding='utf-8') as file_json:
-        data = json.dumps(file_json)
-    if not csv_filename:
+    """ Converts data from a CSV file to JSON format and write
+    it to data.json
+
+    Args:
+    csv_filename (str): the path to the input CSV file
+    Returns:
+    bool: True if the conversion was successful, False otherwise"""
+    try:
+        data_to_serialize = []
+        # Reads the CSV file et appends the data in a list of dictionary
+        with open(csv_filename, 'r', newline='', encoding='utf-8') as f_csv:
+            reader_dict = csv.DictReader(f_csv)
+            for row in reader_dict:
+                data_to_serialize.append(row)
+
+        json_output_filename = "data.json"
+        # Writes the list of dictionary serialized in JSON in data.json
+        with open(json_output_filename, 'w', encoding='utf-8') as file_json:
+            json.dump(data_to_serialize, file_json)
+            return True
+    except FileNotFoundError:
+        return False
+    except Exception:
         return False
