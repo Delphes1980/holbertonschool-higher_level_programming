016bc69a00d2b48fc05bbad28e648fa7d0206c26
Delphes1980 <delphine.coutouly-laborda@hotmail.com>
2025-06-05 11:42:08 +0200

    fix pycodestyle errors

--- python-serialization/task_03_xml.py
+++ python-serialization/task_03_xml.py
@@ -5,17 +5,19 @@ This module defines a method that serialize and deserialize using XML
 
 import xml.etree.ElementTree as ET
 
+
 def serialize_to_xml(dictionary, filename):
-	data = ET.Element(dictionary)
-	for elem in dictionary:
-		ET.SubElement(data, elem)
-	tree = ET.ElementTree(data)
-	with open(filename, 'wb') as f:
-		tree.write(f, encoding='utf-8', xml_declaration=True)
+    data = ET.Element(dictionary)
+    for elem in dictionary:
+        ET.SubElement(data, elem)
+    tree = ET.ElementTree(data)
+    with open(filename, 'wb') as f:
+        tree.write(f, encoding='utf-8', xml_declaration=True)
+
 
 def deserialize_from_xml(filename):
-	tree.ET.parse(filename)
-	root = tree.getroot()
-	dict = {}
-	for item in root.findall:
-		return dict[item]
+    tree.ET.parse(filename)
+    root = tree.getroot()
+    dict = {}
+    for item in root.findall:
+        return dict[item]
