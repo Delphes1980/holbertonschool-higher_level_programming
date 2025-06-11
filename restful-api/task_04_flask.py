3412173e0d850ff92abd6bb305875dee5abe250a
Delphes1980 <delphine.coutouly-laborda@hotmail.com>
2025-06-11 14:43:05 +0200

    Add task_04_flask file
    Define a basic HTTP server to serve API endpoints using Flask

---
+++ restful-api/task_04_flask.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python
+"""
+This module defines a basic HTTP server using Flask to serve API endpoints """
+
+from flask import Flask
+from flask import jsonify
+from flask import request
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    """Returns a welcome message for the API."""
+    return "Welcome to the Flask API!"
+
+
+@app.route('/data', methods=['GET'])
+def get_data():
+    """Returns a list of all available usernames."""
+    return jsonify(list(users.keys()))
+
+
+@app.route('/status', methods=['GET'])
+def status():
+    """Returns the API status (OK)."""
+    return "OK"
+
+
+@app.route('/users/<username>', methods=['GET'])
+def get_user_by_username(username):
+    """
+    Retrieves user data by username.
+    Args:
+        username (str): The username to retrieve.
+    Returns:
+        JSON: User data if found, or an error message with 404 status.
+    """
+    user = users.get(username)
+    if user:
+        users['username'] = username
+        return jsonify(user)
+    else:
+        return (jsonify({"error": "User not found"}), 404)
+
+
+@app.route('/add_user', methods=['POST'])
+def add_user():
+    """ Adds a new user to the system."""
+    user_data = request.get_json()
+    username = user_data.get("username")
+    if username:
+        users[username] = user_data  # Add the new user's data
+        return "New user created", 201
+    else:
+        return {"error": "Username is required"}, 400
+
+
+if __name__ == "__main__":
+    app.run()
