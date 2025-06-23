32ac9af24cc8ed2cd039c71a6cd44c7d284dbcae
Delphes1980 <delphine.coutouly-laborda@hotmail.com>
2025-06-23 19:07:58 +0200

    Add 10-top_score file
    A script that displays choosen columns of the table

---
+++ SQL_introduction/10-top_score.sql
@@ -0,0 +1,4 @@
+-- A script that displays score and name columns of the table second_table of
+-- the database hbtn_0c_0 in my MySQL server
+SELECT DISTINCT score, name FROM second_table
+ORDER BY score DESC;
