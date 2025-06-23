2b8c74360766242c6e466fefa462ced7daf9409b
Delphes1980 <delphine.coutouly-laborda@hotmail.com>
2025-06-23 14:01:26 +0200

    add the condition if not exists to the file 1

--- SQL_introduction/1-create_database_if_missing.sql
+++ SQL_introduction/1-create_database_if_missing.sql
@@ -1,2 +1,2 @@
--- a script that creates a database of my MySQL server
-CREATE DATABASE hbtn_0c_0;
+-- a script that creates a database in my MySQL server if not exists
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
