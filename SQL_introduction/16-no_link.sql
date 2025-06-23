a52b62b249db4f566c4fd7f66773bb7fbaa2d31f
Delphes1980 <delphine.coutouly-laborda@hotmail.com>
2025-06-23 20:23:08 +0200

    add comments

--- SQL_introduction/16-no_link.sql
+++ SQL_introduction/16-no_link.sql
@@ -1,7 +1,7 @@
-/* A script that lists all records of the table second_table of the database
-hbtn_0c_0 in my MySQL server.Don’t list rows where the name column does not
-contain a value. Results should display the score and the name (in this order)
-Records should be listed by descending score */
+-- A script that lists all records of the table second_table of the database
+-- hbtn_0c_0 in my MySQL server.Don’t list rows where the name column does
+-- not contain a value. Results should display the score and the name (in this
+-- order). Records should be listed by descending score
 SELECT score, name FROM second_table
 WHERE NOT name = ''
 ORDER BY score DESC;
