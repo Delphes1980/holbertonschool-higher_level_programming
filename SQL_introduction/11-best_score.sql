5fc1b1c3bd2559c1fafe0178c31e14b83444256d
Delphes1980 <delphine.coutouly-laborda@hotmail.com>
2025-06-23 19:23:06 +0200

    Ad  11-best_score file
    A script that displays the top 2 of the score column

---
+++ SQL_introduction/11-best_score.sql
@@ -0,0 +1,6 @@
+--  A script that lists all records with a score >= 10 in the table
+-- second_table of the database hbtn_0c_0 in my MySQL server
+SELECT score, name 
+FROM second_table
+ORDER BY score DESC
+LIMIT 2;
