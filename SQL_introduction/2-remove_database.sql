4cb75a41f5de33f9d278371e28ea70725666d26c
Delphes1980 <delphine.coutouly-laborda@hotmail.com>
2025-06-23 14:21:33 +0200

    fix a spelling error

--- SQL_introduction/2-remove_database.sql
+++ SQL_introduction/2-remove_database.sql
@@ -1,3 +1,3 @@
 -- a script that deletes a database in my MySQL server
 -- If the database doesn’t exist, the script should not fail
-DROP DATABSE IF EXISTS hbtn_0c_0;
+DROP DATABASE IF EXISTS hbtn_0c_0;
