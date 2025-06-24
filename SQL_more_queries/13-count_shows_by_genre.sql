4c33e4680a6d774aa405bd2893ee44786c349308
Delphes1980 <delphine.coutouly-laborda@hotmail.com>
2025-06-24 20:43:38 +0200

    change left join for inner join

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -3,7 +3,7 @@
 SELECT tv_genres.name AS genre,  -- Selects the NAME of the genre
 COUNT(tv_show_genres.show_id) AS number_of_shows  -- Counts the number of 'show_id' entries linked to each genre
 FROM tv_genres
-LEFT JOIN tv_show_genres
+INNER JOIN tv_show_genres
 ON tv_genres.id = tv_show_genres.genre_id
 GROUP BY genre  -- Groups the results by the genre's NAME
 ORDER BY number_of_shows DESC;
