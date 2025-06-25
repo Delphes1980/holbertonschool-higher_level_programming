2367b207daf8d49b6eadd8d8f0ab6faa3c3171b8
Delphes1980 <delphine.coutouly-laborda@hotmail.com>
2025-06-25 19:33:35 +0200

    add semi colon at the end of the query

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -13,10 +13,14 @@ if __name__ == "__main__":
 
     db = MySQLdb.connect(host='localhost', user='user',
                          passwd='password', database='db_name', port='3306')
+
     cur = db.cursor()
-    cur.execute("SELECT id, name FROM states ORDER BY id ASC")
+
+    cur.execute("SELECT id, name FROM states ORDER BY id ASC;")
     states = cur.fetchall()
+
     for id, name in states:
         print(f"({id}, '{name}'")
+
     cur.close()
     db.close()
