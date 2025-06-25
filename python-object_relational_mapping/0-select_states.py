4266904d30aee445fe9f51e91d131be8dae76f0c
Delphes1980 <delphine.coutouly-laborda@hotmail.com>
2025-06-25 19:30:36 +0200

    Add readme and 0-select_states files
    A script that lists all states from a specified database

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+This module lists all states from a specified database
+"""
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    user = sys.argv[1]
+    password = sys.arg[2]
+    db_name = sys.arg[3]
+
+    db = MySQLdb.connect(host='localhost', user='user',
+                         passwd='password', database='db_name', port='3306')
+    cur = db.cursor()
+    cur.execute("SELECT id, name FROM states ORDER BY id ASC")
+    states = cur.fetchall()
+    for id, name in states:
+        print(f"({id}, '{name}'")
+    cur.close()
+    db.close()
---
+++ python-object_relational_mapping/README.md
@@ -0,0 +1,1 @@
+this is the readme file for Python object relational mapping tasks
