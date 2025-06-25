794690c34827d366ed380f3f8f6345b21a5fef14
Delphes1980 <delphine.coutouly-laborda@hotmail.com>
2025-06-25 20:03:04 +0200

    Add 1-filter_states file
    A script that lists all states starting with a N

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+This module lists all states from a specified database starting with a N
+"""
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    user = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+
+    db = MySQLdb.connect(host='localhost', user=user,
+                         passwd=password, database=db_name, port=3306)
+
+    cur = db.cursor()
+
+    cur.execute("SELECT id, name FROM states WHERE name = N% ORDER BY id ASC")
+    states = cur.fetchall()
+
+    for id, name in states:
+        print(f"({id}, '{name}')")
+
+    cur.close()
+    db.close()
