020c6bda9dfb37dea900dbdcdf0b5225a2fccdde
Delphes1980 <delphine.coutouly-laborda@hotmail.com>
2025-06-25 20:39:04 +0200

    add comments to files

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -7,20 +7,27 @@ import sys
 
 
 if __name__ == "__main__":
+    # Get MySQL credentials and database name from command_line arguments
     user = sys.argv[1]
     password = sys.argv[2]
     db_name = sys.argv[3]
 
+    # Establish database connection
     db = MySQLdb.connect(host='localhost', user=user,
                          passwd=password, database=db_name, port=3306)
 
+    # Create a cursor to execute queries
     cur = db.cursor()
 
+    # SQL query: Selects all 'id' and 'name' from the 'states' table
     cur.execute("SELECT id, name FROM states ORDER BY id ASC;")
+    # Fetch all results from the executed query
     states = cur.fetchall()
 
+    # Print each state
     for id, name in states:
         print(f"({id}, '{name}')")
 
+    # Close the cursor and the database connection
     cur.close()
     db.close()
--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,26 +1,34 @@
 #!/usr/bin/python3
 """
 This module lists all states from a specified database starting with N
+(uppercase)
 """
 import MySQLdb
 import sys
 
 
 if __name__ == "__main__":
+    # Get MySQL credentials and database name from command_line arguments
     user = sys.argv[1]
     password = sys.argv[2]
     db_name = sys.argv[3]
 
+    # Establish database connection
     db = MySQLdb.connect(host='localhost', user=user,
                          passwd=password, database=db_name, port=3306)
 
+    # Create a cursor to execute queries
     cur = db.cursor()
 
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC;")
+    # SQL query: Select all states where name starts with 'N', sorted by id
+    cur.execute("SELECT * FROM states WHERE BINARY name LIKE 'N%' ORDER BY id ASC;")
+    # Fetch all results from the executed query
     states = cur.fetchall()
 
+    # Print each state
     for id, name in states:
         print(f"({id}, '{name}')")
 
+    # Close the cursor and the database connection
     cur.close()
     db.close()
