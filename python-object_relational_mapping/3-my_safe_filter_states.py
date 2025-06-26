d176b656ba44d91ff0d38aeafbf9396e17d166c0
Delphes1980 <delphine.coutouly-laborda@hotmail.com>
2025-06-26 14:41:24 +0200

    delete the format function

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -24,7 +24,7 @@ if __name__ =="__main__":
 	query = """SELECT *
 				FROM states
 				WHERE BINARY name = %s
-				ORDER BY id ASC;""".format(state_name_search)
+				ORDER BY id ASC;"""
 
 	cur.execute(query, (state_name_search,))
 
