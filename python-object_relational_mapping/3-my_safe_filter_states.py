b008def4c0b01c5cb22d4f4735fe54e608ad6e97
Delphes1980 <delphine.coutouly-laborda@hotmail.com>
2025-06-26 13:09:01 +0200

    Add 3-my_safe_filter_states file
    A script where 'state_name_search' is safe from SQL injection

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+This module displays all values in the states table where 'name' matches the
+argument 'state name searched' (safe from MySQL injection) 
+"""
+import MySQLdb
+import sys
+
+
+if __name__ =="__main__":
+	user = sys.argv[1]
+	password = sys.argv[2]
+	db_name = sys.argv[3]
+	state_name_search = sys.argv[4]
+
+	db = MySQLdb.connect(host='localhost',
+						 user=user,
+						 passwd=password,
+						 database=db_name,
+						 port=3306)
+
+	cur = db.cursor()
+
+	query = """SELECT *
+				FROM states
+				WHERE BINARY name = '{}\'
+				ORDER BY id ASC;""".format(state_name_search)
+	cur.execute(query)
+
+	states = cur.fetchall()
+
+	for id, name in states:
+		print(f"({id}, '{name}')")
+
+	cur.close()
+	db.close()
