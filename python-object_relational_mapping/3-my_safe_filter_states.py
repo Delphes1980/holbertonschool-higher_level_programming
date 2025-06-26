3ff55b8ef2c762234b053ddc7586fdf2d953c392
Delphes1980 <delphine.coutouly-laborda@hotmail.com>
2025-06-26 14:43:55 +0200

    fixing pycodestyle errors

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,37 +1,37 @@
 #!/usr/bin/python3
 """
 This module displays all values in the states table where 'name' matches the
-argument 'state name searched' (safe from MySQL injection) 
+argument 'state name searched' (safe from MySQL injection)
 """
 import MySQLdb
 import sys
 
 
-if __name__ =="__main__":
-	user = sys.argv[1]
-	password = sys.argv[2]
-	db_name = sys.argv[3]
-	state_name_search = sys.argv[4]
+if __name__ == "__main__":
+    user = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+    state_name_search = sys.argv[4]
 
-	db = MySQLdb.connect(host='localhost',
-						 user=user,
-						 passwd=password,
-						 database=db_name,
-						 port=3306)
+    db = MySQLdb.connect(host='localhost',
+                         user=user,
+                         passwd=password,
+                         database=db_name,
+                         port=3306)
 
-	cur = db.cursor()
+    cur = db.cursor()
 
-	query = """SELECT *
-				FROM states
-				WHERE BINARY name = %s
-				ORDER BY id ASC;"""
+    query = """SELECT *
+                FROM states
+                WHERE BINARY name = %s
+                ORDER BY id ASC;"""
 
-	cur.execute(query, (state_name_search,))
+    cur.execute(query, (state_name_search,))
 
-	states = cur.fetchall()
+    states = cur.fetchall()
 
-	for id, name in states:
-		print(f"({id}, '{name}')")
+    for id, name in states:
+        print(f"({id}, '{name}')")
 
-	cur.close()
-	db.close()
+    cur.close()
+    db.close()
