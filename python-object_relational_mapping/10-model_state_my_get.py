d1a6725e72bfae25ae245933c1aaf5c296304590
Delphes1980 <delphine.coutouly-laborda@hotmail.com>
2025-06-27 13:55:09 +0200

    Améliorer la lisibilité du message d'utilisation dans le script 10-model_state_my_get.py

--- python-object_relational_mapping/10-model_state_my_get.py
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -13,7 +13,8 @@ import sys
 if __name__ == "__main__":
     # Validate the number of arguments passed
     if len(sys.argv) != 5:
-        print("Usage: {} <user> <password> <db_name> <state_name_to_search>".format(sys.argv[0]))
+        print("Usage: {} <user> <password> <db_name> <state_name_to_search>"
+              .format(sys.argv[0]))
         sys.exit(1)
 
     # Get database connection details from command-line arguments
