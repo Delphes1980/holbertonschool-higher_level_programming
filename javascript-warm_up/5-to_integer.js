c56429ac4aff5b955b6d1dae6132c6378183d129
Delphes1980 <delphine.coutouly-laborda@hotmail.com>
2025-07-13 13:41:28 +0200

    add 5-to_integer.js file

---
+++ javascript-warm_up/5-to_integer.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const { argv } = require('node:process');
+
+// convert the argument to a number
+const arg = parseInt(argv[2]);
+
+// Check if it's a valid number
+if (isNaN(arg)) {
+  console.log('Not a number');
+} else {
+  console.log('My number', arg);
+}
