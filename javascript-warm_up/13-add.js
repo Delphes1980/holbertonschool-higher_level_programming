4871eb9a670d788acfab9cf569322152d7c383df
Delphes1980 <delphine.coutouly-laborda@hotmail.com>
2025-07-13 18:28:16 +0200

    delete a part that was unused

--- javascript-warm_up/13-add.js
+++ javascript-warm_up/13-add.js
@@ -5,12 +5,4 @@ function add (a, b) {
   return a + b;
 }
 
-exports.add = add;
-
-const num1 = parseInt(argv[2]);
-const num2 = parseInt(argv[3]);
-
-if (num1 !== isNaN(num1) && num2 !== isNaN(num2)) {
-  const result = add(num1, num2);
-  console.log(result);
-}
+module.exports.add = add;
