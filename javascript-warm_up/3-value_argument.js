d201f41c79d454aa5c717399ca772329a47f419a
Delphes1980 <delphine.coutouly-laborda@hotmail.com>
2025-07-13 12:37:40 +0200

    try to please the checker

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -4,9 +4,5 @@ const { argv } = require('node:process');
 if (argv[2] == null) {
   console.log('No argument');
 } else {
-  let i = 2;
-  while (argv[i]) {
-    console.log(argv[i]);
-    i++;
-  }
+  console.log(argv[2]);
 }
