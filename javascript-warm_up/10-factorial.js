9471c79fefb0338eaeb5370723d570b22c579fed
Delphes1980 <delphine.coutouly-laborda@hotmail.com>
2025-07-13 16:25:40 +0200

    add a condiion to check if it's a NaN or less than 0

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -3,6 +3,10 @@ const { argv } = require('node:process');
 const fact = parseInt(argv[2]);
 
 function factorial (n) {
+	/* Check if is a NaN or less than 0 */
+  if (isNaN(n) || n < 0) {
+    return 1;
+  }
   if (n === 0) { // Base case
     return 1;
   }
