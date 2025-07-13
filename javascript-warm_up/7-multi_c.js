0d3797f234c42f63e566686b81d4c078fe5e040e
Delphes1980 <delphine.coutouly-laborda@hotmail.com>
2025-07-13 14:22:17 +0200

    define the variable i in the loop function

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -6,7 +6,7 @@ const arg = parseInt(argv[2]);
 if (isNaN(arg)) {
   console.log('Missing number of occurrences');
 } else {
-  for (i = 0; i < arg; i++) {
+  for (let i = 0; i < arg; i++) {
     console.log(line);
   }
 }
